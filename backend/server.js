e0e6d2745ea16880709f25f8ceb54c0aa0c41083
10billionpercent <103302612+10billionpercent@users.noreply.github.com>
2025-11-12 20:47:11 +0530

    Initialize Express server with MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js"; 
+import documentRoutes from "./routes/documentRoutes.js"; 
+dotenv.config();
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json()); 
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/documents", documentRoutes); 
+
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
