31d527da816d1a9598856fdb0dca7ffdd759b4c1
10billionpercent <103302612+10billionpercent@users.noreply.github.com>
2025-11-12 20:45:41 +0530

    Create User model with schema definition

---
+++ backend/models/User.js
@@ -0,0 +1,11 @@
+// models/User.js
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, 
+  role: { type: String, default: "user" },
+  email: { type: String, required: true, unique: true },
+});
+
+export default mongoose.model("User", userSchema);
